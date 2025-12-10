6c38a7826018a469f299a2e5cec137a934bf9a80
Mantissagithub <mantissa6789@gmail.com>
2025-12-11 00:29:52 +0530

    Adding a better implementation of kv cache with ordered dict

--- utils/kv_cache.py
+++ utils/kv_cache.py
@@ -1,40 +1,62 @@
 # this is the file i'm gonna code up the kv cache with lru eviction strategy
-# when i think in first principles it's just a set of values like {keys[i], values[i], timestamp[i]} where timestamp is the last used time
+# when i think in first principles it's just a set of values like [{keys, values, timestamp}] where timestamp is the last used time
 # also we can store that in the constatn mem or the hbm as obviously pytorch uses the gpu
 
 import time
 import torch
-import sys
+# import sys
+import torch.nn as nn
+from collections import OrderedDict
 
-class KVCache:
+class KVCache(nn.Module):
   def __init__(self, max_size):
+    super().__init__()
     self.max_size = max_size
-    self.cache = [] # [key, value, timestamp]
-
-  def put_into_cuda_const_mem(self):
-    if not torch.cuda.is_available():
-      print("either pytorch is not able to access the gpu, or there is no gpu!!")
-      return
-
-    self.register_buffer('kv_cache', self.cache)
-
-  def sort_lru(self):
-    self.cache.sort(key=self.cache[2]).reverse()
+    # self.cache = [] # [key, value, timestamp]
+    self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    self.cache = OrderedDict() # (key) -> (value, timestamp)
 
   def get_by_key(self, key):
-    for x in self.cache:
-      if key == x[0]:
-        print(f"found the key: {key}, and the value is {x[1]}")
-        return x[1]
-    print(f"key: {key} not found!!")
-    sys.exit(0)
+    if key not in self.cache:
+      print(f"key: {key} not found!!")
+      return None
+
+    value, _ = self.cache[key]
+    current_time_ms = int(time.time() * 1000)
+    self.cache[key] = (value, current_time_ms)
+    print(f"found key: {key} with value: {value}")
+    return value
 
   def put(self, key, value):
     current_time_ms = int(time.time()*1000)
-    self.cache.append([key, value, current_time_ms])
+    if key in self.cache:
+      self.cache.move_to_end(key)
+      self.cache[key] = (value, current_time_ms)
+      print(f"key: {key} already found, updated!!")
+      return
+
+    self.cache[key] = (value, current_time_ms)
+
     if len(self.cache) > self.max_size:
-      self.sort_lru()
-      self.cache.pop()
-      print("the least recently used element is popped!!")
-    print(f"added key; {key}, value: {value} at timestamp in ms: {current_time_ms}")
+      lru_cache = next(iter(self.cache))
+      self.cache.pop(lru_cache)
+      print(f"lru evicted: {lru_cache}")
+
+    print(f"successfully added key: {key}, value: {value} with timestamp_ms: {current_time_ms}")
+
+  def to_gpu(self):
+    if self.device == 'cuda':
+      # i can use torch.tensor over here
+      gpu_cache = OrderedDict()
+      for k, (v, ts) in self.cache.items():
+        if not isinstance(v, torch.Tensor):
+          v = torch.tensor(v, device=self.device)
+        else:
+          v = v.to(self.device)
+        gpu_cache[k] = (v, ts)
+      self.cache = gpu_cache
+      print("moved to gpu")
+    else:
+      print("gpu cannot be found!!")
+
 
